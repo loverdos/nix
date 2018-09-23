b199557031889d117b95d38c0cd9006a111f0672
Christos KK Loverdos <loverdos@gmail.com>
2018-09-23 22:08:05 +0300

    dockerize

---
+++ nix-install-env.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# This is meant to be used from the Dockerfile
+# See also https://github.com/loverdos/ubuntu.nix
+
+set -eux
+
+cd ~ \
+&& git clone https://github.com/loverdos/nix.git \
+&& cd nix \
+&& nix-env -v -iA install -f default.nix
