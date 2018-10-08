3ff62b4399934967c146f6cb119e522f21dc1b14
Christos KK Loverdos <loverdos@gmail.com>
2018-10-08 22:27:52 +0300

    pl/python: add python3

--- pl/python/default.nix
+++ pl/python/default.nix
@@ -1,21 +1,29 @@
 { pkgs /*: NixPkgs*/, devenv }:
 
 let
-  python3 = pkgs.python36;
+  py3pkgs = pkgs.python36Packages;
 in
   pkgs.buildEnv {
     name = "${devenv.name}-python3";
-    paths = with python3.pkgs; [
+    paths = [
+      pkgs.python3
+    ] ++ (with py3pkgs; 
+    [
       pygments
 
+      setuptools
+      tkinter
+
+      # console
       bpython
       ipython
       ptpython
 
+      # schema
       protobuf
       thrift
 
       # issue tracking
       bugwarrior
-    ];
+    ]);
   }
