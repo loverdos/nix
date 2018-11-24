d820740a82e7f16962d33a10c1e63f7faa0ba6b5
Christos KK Loverdos <loverdos@gmail.com>
2018-11-24 21:31:18 +0200

    custom: refactor emojify to look like the others

--- custom/emojify.nix
+++ custom/emojify.nix
@@ -1,23 +1,23 @@
 { pkgs /*: NixPkgs */}:
 
 let
-  version = "2.0.0";
+  owner = "mrowa44";
+  repo = "emojify";
+  binary = repo;
+  rev = "2.0.0";
   sha256 = "0zhbfxabgllpq3sy0pj5mm79l24vj1z10kyajc4n39yq8ibhq66j";
 in
   pkgs.stdenv.mkDerivation {
-    name = "emojify-${version}";
+    name = "${binary}-${rev}";
 
     src = pkgs.fetchFromGitHub {
-      owner = "mrowa44";
-      repo = "emojify";
-      rev = "${version}";
-      sha256 = "${sha256}";
+      inherit owner repo rev sha256;
     };
 
     installPhase = ''
       mkdir -p $out/bin
 
-      cp emojify $out/bin/
-      chmod +x $out/bin/emojify
+      cp ${binary} $out/bin/
+      chmod +x $out/bin/${binary}
     '';
   }
